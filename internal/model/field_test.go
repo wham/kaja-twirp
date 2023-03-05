1e67d39da04e46400d121cca054267f9749d0b24
Tomáš Veselý <wham@github.com>
2023-03-04 22:10:33 -0800

    Generate test data

--- internal/model/field_test.go
+++ internal/model/field_test.go
@@ -5,10 +5,15 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 func TestFormatTime(t *testing.T) {
 	p, _ := time.Parse(time.RFC3339, "2014-11-12T11:45:26.371Z")
 
 	assert.Equal(t, "2014-11-12T00:00:00Z", FormatTime(p))
+}
+
+func TestGetDefaultValue(t *testing.T) {
+	field := protoreflect.FieldDescriptor{}
 }
