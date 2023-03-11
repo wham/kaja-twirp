dd36fa541f79263aaf5c78b755911fc62e9087fb
Tomáš Veselý <wham@github.com>
2023-03-10 18:38:07 -0800

    Added tests

--- internal/twirp/twirp.go
+++ internal/twirp/twirp.go
@@ -1,5 +1,24 @@
 package twirp
 
+import (
+	"net/url"
+	"os"
+)
+
 func GetBaseURL() string {
-	return "hi"
+	baseURL := os.Getenv("BASE_URL")	
+	return sanitizeBaseURL(baseURL)
+}
+
+// sanitizeBaseURL parses the the baseURL, and adds the "http" scheme if needed.
+// If the URL is unparsable, the baseURL is returned unchaged.
+func sanitizeBaseURL(baseURL string) string {
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		return baseURL // invalid URL will fail later when making requests
+	}
+	if u.Scheme == "" {
+		u.Scheme = "http"
+	}
+	return u.String()
 }
