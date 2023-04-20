c4fbfc792a4b3737a8b62b23974053ce5d19522c
Tomáš Veselý <wham@github.com>
2023-04-19 23:16:15 -0700

    call

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { Sidebar } from './Sidebar';
 import Editor from '@monaco-editor/react';
+import { QuirksClient } from './quirks.client';
+import { TwirpFetchTransport } from '@protobuf-ts/twirp-transport';
+import { SearchService } from './search-service';
+import { SearchServiceClient } from './search-service.client';
 
 function App() {
   useEffect(() => {
@@ -29,9 +33,26 @@ function App() {
         </a>
       </header>
       <Sidebar/>
+      <button onClick={ callApi }>Call</button>
       <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" />
     </div>
   );
+}
+
+async function callApi() {
+  let transport = new TwirpFetchTransport({
+    baseUrl: "http://localhost:41521"
+  });
+
+  let client = new SearchServiceClient(transport)
+
+  let {response} = await client.search({
+    query: '',
+    pageNumber: 0,
+    resultPerPage: 0
+  })
+
+  alert(JSON.stringify(response))
 }
 
 export default App;
