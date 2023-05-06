d7949a921a1606f77a5201f0d1208bf96784e383
Tomáš Veselý <wham@github.com>
2023-05-05 23:47:19 -0700

    working eval

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -8,13 +8,48 @@ import { TwirpFetchTransport } from '@protobuf-ts/twirp-transport';
 import { SearchService } from './search-service';
 import { SearchServiceClient } from './search-service.client';
 
+const xSearchService = {
+  search: async function() {
+    let transport = new TwirpFetchTransport({
+      baseUrl: "http://localhost:3000/twirp"
+    });
+  
+    let client = new SearchServiceClient(transport)
+  
+    let {response} = await client.search({
+      query: '',
+      pageNumber: 0,
+      resultPerPage: 0
+    })
+    
+    alert(JSON.stringify(response));
+  }
+}
+
 function App() {
+  const editorRef = React.useRef(null);
+
   useEffect(() => {
     fetch("/api/model")
       .then((response) => {
         console.log(response)
       })
   })
+
+  function handleEditorDidMount(editor: any, monaco: any) {
+    editorRef.current = editor;
+    editor.focus();
+  }
+
+  async function callApi() {
+    //let response = await xSearchService.search()
+  
+    if (editorRef.current)  {
+      eval((editorRef.current as any).getValue());
+    }
+    //alert(JSON.stringify(response));
+    //alert(JSON.stringify(await xSearchService.search()))
+  }
 
   return (
     <div className="App">
@@ -34,25 +69,9 @@ function App() {
       </header>
       <Sidebar/>
       <button onClick={ callApi }>Call</button>
-      <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" />
+      <Editor height="90vh" defaultLanguage="javascript" defaultValue="xSearchService.search();" onMount={handleEditorDidMount} />
     </div>
   );
-}
-
-async function callApi() {
-  let transport = new TwirpFetchTransport({
-    baseUrl: "http://localhost:3000/twirp"
-  });
-
-  let client = new SearchServiceClient(transport)
-
-  let {response} = await client.search({
-    query: '',
-    pageNumber: 0,
-    resultPerPage: 0
-  })
-
-  alert(JSON.stringify(response))
 }
 
 export default App;
