72fa442c741fb281be61386b756fc793634f1974
Tomáš Veselý <wham@github.com>
2023-05-06 23:01:19 -0700

    removed logo

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Sidebar } from './Sidebar';
 import Editor from '@monaco-editor/react';
@@ -52,21 +51,7 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
       <Sidebar/>
       <button onClick={ callApi }>Call</button>
       <Editor height="90vh" defaultLanguage="javascript" defaultValue="xSearchService.search();" onMount={handleEditorDidMount} />
