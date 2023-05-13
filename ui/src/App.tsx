410dcd2c04827fe5653548e6370d1d4936ec0003
Tomáš Veselý <wham@github.com>
2023-05-12 23:05:32 -0700

    content

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Sidebar } from "./Sidebar";
-import Editor from "@monaco-editor/react";
-import { QuirksClient } from "./quirks.client";
-import { TwirpFetchTransport } from "@protobuf-ts/twirp-transport";
-import { SearchService } from "./search-service";
-import { SearchServiceClient } from "./search-service.client";
 import { Model } from "./Model";
 import { Box, ThemeProvider } from "@primer/react";
-
-const xSearchService = {
-  search: async function () {
-    let transport = new TwirpFetchTransport({
-      baseUrl: "http://localhost:3000/twirp",
-    });
-
-    let client = new SearchServiceClient(transport);
-
-    let { response } = await client.search({
-      query: "",
-      pageNumber: 0,
-      resultPerPage: 0,
-    });
-
-    alert(JSON.stringify(response));
-  },
-};
+import { Content } from "./Content";
 
 function App() {
-  const editorRef = React.useRef(null);
-
   const [model, setModel] = useState<Model>({ Files: [] });
 
   useEffect(() => {
@@ -42,21 +18,6 @@ function App() {
       });
   }, []);
 
-  function handleEditorDidMount(editor: any, monaco: any) {
-    editorRef.current = editor;
-    editor.focus();
-  }
-
-  async function callApi() {
-    //let response = await xSearchService.search()
-
-    if (editorRef.current) {
-      eval((editorRef.current as any).getValue());
-    }
-    //alert(JSON.stringify(response));
-    //alert(JSON.stringify(await xSearchService.search()))
-  }
-
   return (
     <ThemeProvider colorMode="night">
       <Box sx={{ display: "flex", height: "100vh", bg: "canvas.default" }}>
@@ -64,13 +25,7 @@ function App() {
           <Sidebar model={model} />
         </Box>
         <Box sx={{ flexGrow: 1 }}>
-          <button onClick={callApi}>Call</button>
-          <Editor
-            height="90vh"
-            defaultLanguage="javascript"
-            defaultValue="xSearchService.search();"
-            onMount={handleEditorDidMount}
-          />
+          <Content />
         </Box>
       </Box>
     </ThemeProvider>
