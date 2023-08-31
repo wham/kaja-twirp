ea2db47eda4dabf9f5cc2959ae2d8df34a6a7605
Tomáš Veselý <wham@github.com>
2023-08-30 23:28:32 -0700

    New plugin

--- plugin/src/plugin.ts
+++ plugin/src/plugin.ts
@@ -1,68 +1,96 @@
 import * as ts from "typescript";
-import { CodeGeneratorRequest, GeneratedFile, PluginBase, TypescriptFile } from "@protobuf-ts/plugin-framework";
+import {
+  CodeGeneratorRequest,
+  GeneratedFile,
+  PluginBase,
+  TypescriptFile,
+} from "@protobuf-ts/plugin-framework";
 
 export class Plugin extends PluginBase {
+  constructor() {
+    //console.log('Hello from plugin constructor!');
+    super();
+  }
 
-    constructor() {
-        //console.log('Hello from plugin constructor!');
-        super();
-    }
-    
+  // https://github.dev/timostamm/protobuf-ts
+  generate(
+    request: CodeGeneratorRequest
+  ): GeneratedFile[] | Promise<GeneratedFile[]> {
+    let file = new TypescriptFile("kaja-twirp.ts");
+    const file2 = ts.createSourceFile(
+      "source.ts",
+      "",
+      ts.ScriptTarget.ESNext,
+      false,
+      ts.ScriptKind.TS
+    );
+    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+
+    const statement = ts.createVariableStatement(
+      [ts.createToken(ts.SyntaxKind.ExportKeyword)],
+      ts.createVariableDeclarationList(
+        [
+          ts.createVariableDeclaration(
+            ts.createIdentifier("model"),
+            ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+            ts.createStringLiteral(
+              printer.printNode(
+                ts.EmitHint.Unspecified,
+                this.services(request),
+                file2
+              )
+            )
+          ),
+        ],
+        ts.NodeFlags.Const
+      )
+    );
+
+    //console.log('Hello from plugin!');
     // https://github.dev/timostamm/protobuf-ts
-    generate(request: CodeGeneratorRequest): GeneratedFile[] | Promise<GeneratedFile[]> {
-        let file = new TypescriptFile("kaja-twirp.ts");
-        const file2 = ts.createSourceFile("source.ts", "", ts.ScriptTarget.ESNext, false, ts.ScriptKind.TS);
-        const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+    return [file];
+  }
 
-        for (let protoFile of request.protoFile) {
-            for (let service of protoFile.service) {
-                if (!service.name) {
-                    continue;
-                }
+  private services(request: CodeGeneratorRequest): ts.ArrayLiteralExpression {
+    const services = [];
 
-                const methodFuncs = [];
-
-                for (let method of service.method) {
-                    if (!method.name) {
-                        continue;
-                    }
-
-                    const functionDeclaration = ts.createFunctionExpression(
-                        undefined,
-                        undefined,
-                        ts.createIdentifier('add'),
-                        undefined,
-                        [ts.createParameter(undefined, undefined, undefined, 'a', undefined, ts.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword)), ts.createParameter(undefined, undefined, undefined, 'b', undefined, ts.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword))],
-                        ts.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
-                        ts.createBlock([
-                          ts.createReturn(ts.createBinary(ts.createIdentifier('a'), ts.SyntaxKind.PlusToken, ts.createIdentifier('b')))
-                        ])
-                      );
-                    
-                    methodFuncs.push(ts.createPropertyAssignment(method.name, functionDeclaration));
-
-                }
-
-                const objectLiteral = ts.createObjectLiteral(methodFuncs);
-                
-                const statement = ts.createVariableStatement(
-                    [ts.createToken(ts.SyntaxKind.ExportKeyword)],
-                    ts.createVariableDeclarationList(
-                        [ts.createVariableDeclaration(
-                            ts.createIdentifier(service.name),
-                            ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
-                            ts.createStringLiteral(printer.printNode(ts.EmitHint.Unspecified, objectLiteral, file2))
-                        )],
-                        ts.NodeFlags.Const
-                    )
-                );
-        
-                file.addStatement(statement);
-            }
+    for (let protoFile of request.protoFile) {
+      for (let protoService of protoFile.service) {
+        if (!protoService.name) {
+          continue;
         }
-        
-        //console.log('Hello from plugin!');
-        // https://github.dev/timostamm/protobuf-ts
-        return [file];
+
+        const methods = [];
+
+        for (let protoMethod of protoService.method) {
+          if (!protoMethod.name) {
+            continue;
+          }
+
+          const p = ts.createPropertyAssignment(
+            "name",
+            ts.createStringLiteral(protoMethod.name)
+          );
+          const method = ts.createObjectLiteral([p]);
+
+          methods.push(method);
+        }
+
+        const service = ts.createObjectLiteral([
+          ts.createPropertyAssignment(
+            "name",
+            ts.createStringLiteral(protoService.name)
+          ),
+          ts.createPropertyAssignment(
+            "methods",
+            ts.createArrayLiteral(methods)
+          ),
+        ]);
+
+        services.push(service);
+      }
     }
+
+    return ts.createArrayLiteral(services);
+  }
 }
