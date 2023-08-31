e79c51830f47d1568b612c0c9ed20c03e1625632
Tomáš Veselý <wham@github.com>
2023-08-30 23:45:58 -0700

    New model

--- ui/src/Sidebar.tsx
+++ ui/src/Sidebar.tsx
@@ -10,30 +10,28 @@ export function Sidebar({ model, onSelect }: SidebarProps) {
   return (
     <nav aria-label="Methods">
       <TreeView aria-label="Methods">
-        {model.Files.map((file) => {
-          return file.Services.map((service) => {
-            return (
-              <TreeView.Item id={service.Name} key={service.Name} expanded={true}>
-                <TreeView.LeadingVisual>
-                  <TreeView.DirectoryIcon />
-                </TreeView.LeadingVisual>
-                {service.Name}
-                <TreeView.SubTree>
-                  {service.Methods.map((method) => {
-                    return (
-                      <TreeView.Item
-                        id={method.Name}
-                        key={method.Name}
-                        onSelect={() => onSelect(service, method)}
-                      >
-                        {method.Name}
-                      </TreeView.Item>
-                    );
-                  })}
-                </TreeView.SubTree>
-              </TreeView.Item>
-            );
-          });
+        {model.services.map((service) => {
+          return (
+            <TreeView.Item id={service.name} key={service.name} expanded={true}>
+              <TreeView.LeadingVisual>
+                <TreeView.DirectoryIcon />
+              </TreeView.LeadingVisual>
+              {service.name}
+              <TreeView.SubTree>
+                {service.methods.map((method) => {
+                  return (
+                    <TreeView.Item
+                      id={method.name}
+                      key={method.name}
+                      onSelect={() => onSelect(service, method)}
+                    >
+                      {method.name}
+                    </TreeView.Item>
+                  );
+                })}
+              </TreeView.SubTree>
+            </TreeView.Item>
+          );
         })}
       </TreeView>
     </nav>
