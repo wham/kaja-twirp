6353857594e5eacc83bd13ffdae47ebdc60ffd74
Tomáš Veselý <wham@github.com>
2023-09-01 23:47:17 -0700

    proxy

--- ui/src/kaja-twirp.ts
+++ ui/src/kaja-twirp.ts
@@ -16,6 +16,16 @@ export const model = {
         { name: "Repeated", code: 'Quirks.Repeated("argument");\n' },
         { name: "Types", code: 'Quirks.Types("argument");\n' },
       ],
+      proxy: {
+        GetAuthentication: (hello) => 2,
+        Map: (hello) => 2,
+        MethodWithAReallyLongNameGmthggupcbmnphflnnvu: (hello) => 2,
+        Panic: (hello) => 2,
+        Repeated: (hello) => 2,
+        Types: (hello) => 2,
+      },
+      extraLib:
+        "const Quirks = { GetAuthentication: hello => 2, Map: hello => 2, MethodWithAReallyLongNameGmthggupcbmnphflnnvu: hello => 2, Panic: hello => 2, Repeated: hello => 2, Types: hello => 2 };\n",
     },
     {
       name: "SearchService",
@@ -23,10 +33,9 @@ export const model = {
         { name: "Search", code: 'SearchService.Search("argument");\n' },
         { name: "Index", code: 'SearchService.Index("argument");\n' },
       ],
+      proxy: { Search: (hello) => 2, Index: (hello) => 2 },
+      extraLib:
+        "const SearchService = { Search: hello => 2, Index: hello => 2 };\n",
     },
-  ],
-  extraLibs: [
-    "const Quirks = { GetAuthentication: hello => 2, Map: hello => 2, MethodWithAReallyLongNameGmthggupcbmnphflnnvu: hello => 2, Panic: hello => 2, Repeated: hello => 2, Types: hello => 2 };\n",
-    "const SearchService = { Search: hello => 2, Index: hello => 2 };\n",
   ],
 };
