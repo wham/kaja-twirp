e46ca705eb42ecd5bb8b12a891e0a388990cb764
Tomáš Veselý <wham@github.com>
2023-09-03 23:16:29 -0700

    npm start

--- genpick/src/genpick.ts
+++ genpick/src/genpick.ts
@@ -1,3 +1,21 @@
+import * as fs from "fs";
+
 export function main() {
-  console.log("Hello World");
+  const directoryPath = process.argv[2];
+
+  if (!directoryPath) {
+    console.error("No directory path provided");
+    return;
+  }
+
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
 }
