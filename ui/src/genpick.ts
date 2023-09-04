82b93a69d15f20f566071622a9a81cb8d54852bc
Tomáš Veselý <wham@github.com>
2023-09-03 23:57:04 -0700

    List services

--- ui/src/genpick.ts
+++ ui/src/genpick.ts
@@ -1,9 +1,34 @@
-import { Model } from "./Model";
+import ts from "typescript";
+import { Model, Service } from "./Model";
 import { model } from "./gen/kt";
 
 export function loadModel(): Model {
+  const services: Service[] = [];
+
+  model.gens.forEach((gen) => {
+    const sourceFile = ts.createSourceFile(
+      gen.path,
+      gen.content,
+      ts.ScriptTarget.Latest
+    );
+
+    const interfaces = sourceFile.statements.filter(
+      (statement): statement is ts.InterfaceDeclaration =>
+        ts.isInterfaceDeclaration(statement)
+    );
+
+    interfaces.forEach((interfaceDeclaration) => {
+      services.push({
+        name: interfaceDeclaration.name.text,
+        methods: [],
+        proxy: "",
+        extraLib: "",
+      });
+    });
+  });
+
   return {
-    services: [],
+    services,
     extraLibs: [],
   };
 }
