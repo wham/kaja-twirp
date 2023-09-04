0791eaf4b78fe64843e1b42dbdec7a8aff79e617
Tomáš Veselý <wham@github.com>
2023-09-03 23:40:23 -0700

    include files

--- genpick/src/genpick.ts
+++ genpick/src/genpick.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as ts from "typescript";
 
 export function main() {
   const directoryPath = process.argv[2];
@@ -8,14 +9,46 @@ export function main() {
     return;
   }
 
-  fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-      console.error(err);
+  const files = fs.readdirSync(directoryPath);
+  const gens: ts.ObjectLiteralExpression[] = [];
+
+  files.forEach((file) => {
+    if (file === "kt.ts") return;
+
+    let content: string;
+    try {
+      content = fs.readFileSync(directoryPath + "/" + file, "utf-8");
+    } catch (_) {
       return;
     }
 
-    files.forEach((file) => {
-      console.log(file);
-    });
+    gens.push(
+      ts.factory.createObjectLiteralExpression([
+        ts.factory.createPropertyAssignment("path", ts.factory.createStringLiteral(file)),
+        ts.factory.createPropertyAssignment("content", ts.factory.createStringLiteral(content)),
+      ])
+    );
   });
+
+  const model = ts.factory.createVariableStatement(
+    [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
+    ts.factory.createVariableDeclarationList(
+      [
+        ts.factory.createVariableDeclaration(
+          ts.factory.createIdentifier("model"),
+          undefined,
+          undefined,
+          ts.factory.createObjectLiteralExpression([ts.factory.createPropertyAssignment("gens", ts.factory.createArrayLiteralExpression(gens))])
+        ),
+      ],
+      ts.NodeFlags.Const
+    )
+  );
+
+  let sourceFile = ts.createSourceFile("kt.ts", "", ts.ScriptTarget.Latest, /*setParentNodes*/ false, ts.ScriptKind.TS);
+  sourceFile = ts.factory.updateSourceFile(sourceFile, [model]);
+  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+  //console.log(printer.printFile(sourceFile));
+
+  fs.writeFileSync(directoryPath + "/kt.ts", printer.printFile(sourceFile));
 }
