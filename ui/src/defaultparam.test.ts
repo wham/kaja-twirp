97049458abc8d85e5bf3944c945e9cabae717f7f
Tomáš Veselý <wham@github.com>
2023-09-12 23:30:11 -0700

    added debug conf

--- ui/src/defaultparam.test.ts
+++ ui/src/defaultparam.test.ts
@@ -1,3 +1,34 @@
+import ts from "typescript";
+
+const sourceFileContent = `export interface Result {
+  /**
+   * @generated from protobuf field: string url = 1;
+   */
+  url: string;
+  /**
+   * @generated from protobuf field: string title = 2;
+   */
+  title: string;
+  /**
+   * @generated from protobuf field: repeated string snippets = 3;
+   */
+  snippets: string[];
+  /**
+   * @generated from protobuf field: bool is_ad = 4;
+   */
+  isAd: boolean;
+}`;
+
 test("defaultParam", () => {
-  expect(1).toBe(2);
+  const sourceFile = ts.createSourceFile(
+    "test.ts",
+    sourceFileContent,
+    ts.ScriptTarget.Latest
+  );
+
+  const interfaces = sourceFile.statements.filter((statement) => {
+    return ts.isInterfaceDeclaration(statement);
+  });
+
+  expect(1).toBe(interfaces);
 });
