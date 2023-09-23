3caf425a3c06c5ddf3315c4610fdcf4c8dbd4d7f
Tomáš Veselý <wham@github.com>
2023-09-22 23:57:26 -0700

    working proxy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.use("/twirp", createProxyMiddleware({ target: "http://localhost:41521", changeOrigin: true }));
+app.use("/", express.static("../ui/build"));
+
+/*app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
-});
+});*/
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
