af7fdb7152908ef3ca6bfc5b6b4fb36863f26ab7
Tomáš Veselý <wham@github.com>
2023-09-23 23:19:59 -0700

    BASE_URL

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import * as dotenv from "dotenv";
+dotenv.config({ path: process.cwd() + "/../.env" });
 
 const app = express();
 const port = 3000;
 
-app.use("/twirp", createProxyMiddleware({ target: "http://localhost:41521", changeOrigin: true }));
+console.log("BASE_URL", process.env.BASE_URL);
+
+app.use("/twirp", createProxyMiddleware({ target: process.env.BASE_URL, changeOrigin: true }));
 app.use("/", express.static("../ui/build"));
 
 /*app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
 });*/
+
+// BASE_URL
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
