6679d9e843756207e21051c3f9c45d6b0475ba27
Tomáš Veselý <wham@github.com>
2023-10-02 22:30:50 -0700

    Working proxy with vite

--- vite-express-project/src/server/main.ts
+++ vite-express-project/src/server/main.ts
@@ -1,12 +1,11 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 import ViteExpress from "vite-express";
+import * as dotenv from "dotenv";
+dotenv.config({ path: process.cwd() + "/../.env" });
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
+app.use("/twirp", createProxyMiddleware({ target: process.env.BASE_URL, changeOrigin: true }));
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
-);
+ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
