5344a74ddb406cc1be14309fa89aab4dbc3ff0e1
Tomáš Veselý <wham@github.com>
2023-10-06 23:50:00 -0700

    Working protoc

--- ui/src/server/main.ts
+++ ui/src/server/main.ts
@@ -2,10 +2,29 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import ViteExpress from "vite-express";
 import * as dotenv from "dotenv";
+import { exec } from "child_process";
+import path from "path";
 dotenv.config({ path: process.cwd() + "/../.env" });
 
 const app = express();
 
 app.use("/twirp", createProxyMiddleware({ target: process.env.BASE_URL, changeOrigin: true }));
+
+app.use("/bootstrap", (req, res) => {
+  console.log(process.cwd());
+  const protoPath = path.resolve(process.cwd(), "../proto");
+  const outPath = path.resolve(process.cwd(), "../ui/src/client/gen");
+  console.log("protoPath", protoPath);
+
+  exec(`protoc --ts_out ${outPath} --ts_opt long_type_string -I${protoPath} $(find ${protoPath} -iname "*.proto")`, (error, stdout, stderr) => {
+    if (error) {
+      res.send(`exec error: ${error}`);
+      return;
+    }
+
+    res.send(`stdout: ${stdout}`);
+    //console.error(`stderr: ${stderr}`);
+  });
+});
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
