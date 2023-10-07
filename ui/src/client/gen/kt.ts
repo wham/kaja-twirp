1df5d7bf54ad1d2a803ca27fa58cc4d14ebfbbf1
Tomáš Veselý <wham@github.com>
2023-10-07 12:51:09 -0700

    Empy kt.ts

---
+++ ui/src/client/gen/kt.ts
@@ -0,0 +1,7 @@
+import { RpcTransport, ServiceInfo } from "@protobuf-ts/runtime-rpc";
+export const model = {
+  gens: [],
+};
+export function getClient(name: string, transport: RpcTransport): ServiceInfo | undefined {
+  return undefined;
+}
