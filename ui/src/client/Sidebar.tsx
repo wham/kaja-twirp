c876306df3286b1257696098c94b02d0db1274cb
Tomáš Veselý <wham@github.com>
2023-10-14 23:11:33 -0700

    side

--- ui/src/client/Sidebar.tsx
+++ ui/src/client/Sidebar.tsx
@@ -1,5 +1,6 @@
-import { TreeView } from "@primer/react";
+import { Box, Link, TreeView } from "@primer/react";
 import { Method, Model, Service, methodId } from "./model";
+import { Icon, MarkGithubIcon } from "@primer/octicons-react";
 
 type SidebarProps = {
   model: Model;
@@ -9,25 +10,35 @@ type SidebarProps = {
 
 export function Sidebar({ model, onSelect, currentMethod }: SidebarProps) {
   return (
-    <nav aria-label="Services and methods">
-      <TreeView aria-label="Services and methods">
-        {model.services.map((service, index) => {
-          return (
-            <TreeView.Item id={service.name} defaultExpanded={index === 0}>
-              {service.name}
-              <TreeView.SubTree>
-                {service.methods.map((method) => {
-                  return (
-                    <TreeView.Item id={methodId(service, method)} onSelect={() => onSelect(service, method)} current={currentMethod === method}>
-                      {method.name}
-                    </TreeView.Item>
-                  );
-                })}
-              </TreeView.SubTree>
-            </TreeView.Item>
-          );
-        })}
-      </TreeView>
-    </nav>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box sx={{ paddingX: 2, flexGrow: 1 }}>
+        <nav aria-label="Services and methods">
+          <TreeView aria-label="Services and methods">
+            {model.services.map((service, index) => {
+              return (
+                <TreeView.Item id={service.name} defaultExpanded={index === 0}>
+                  {service.name}
+                  <TreeView.SubTree>
+                    {service.methods.map((method) => {
+                      return (
+                        <TreeView.Item id={methodId(service, method)} onSelect={() => onSelect(service, method)} current={currentMethod === method}>
+                          {method.name}
+                        </TreeView.Item>
+                      );
+                    })}
+                  </TreeView.SubTree>
+                </TreeView.Item>
+              );
+            })}
+          </TreeView>
+        </nav>
+      </Box>
+      <Box sx={{ backgroundColor: "accent.emphasis", position: "fixed", bottom: 0, width: 300 }}>
+        <Link href="https://github.com/wham/kaja-twirp">
+          <MarkGithubIcon />
+          kaja-twirp
+        </Link>
+      </Box>
+    </Box>
   );
 }
