60344bc7e06ff320c3fbeb3d10b25701bdc28d99
Tomáš Veselý <wham@github.com>
2023-10-14 23:27:35 -0700

    blank

--- ui/src/client/Sidebar.tsx
+++ ui/src/client/Sidebar.tsx
@@ -34,7 +34,7 @@ export function Sidebar({ model, onSelect, currentMethod }: SidebarProps) {
         </nav>
       </Box>
       <Box sx={{ position: "fixed", bottom: 0, width: 300, borderTopWidth: 1, borderTopStyle: "solid", borderTopColor: "border.default" }}>
-        <Link href="https://github.com/wham/kaja-twirp" sx={{ color: "btn.primary.text", paddingX: 2, paddingY: 2, fontSize: "12px" }}>
+        <Link href="https://github.com/wham/kaja-twirp" sx={{ color: "btn.primary.text", paddingX: 2, paddingY: 2, fontSize: "12px" }} target="_blank">
           <MarkGithubIcon />
           <Text sx={{ paddingLeft: 2 }}>kaja-twirp</Text>
         </Link>
