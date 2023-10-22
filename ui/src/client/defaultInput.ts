0fa455682b45d9dd3d13e93930587d12ea37ad3a
Tomáš Veselý <wham@github.com>
2023-10-22 16:54:43 -0700

    array type

--- ui/src/client/defaultInput.ts
+++ ui/src/client/defaultInput.ts
@@ -44,5 +44,10 @@ function defaultValue(type: ts.TypeNode): ts.Expression {
     return ts.factory.createNumericLiteral("0");
   }
 
+  if (type.kind === ts.SyntaxKind.ArrayType) {
+    const arrayType = type as ts.ArrayTypeNode;
+    return ts.factory.createArrayLiteralExpression([defaultValue(arrayType.elementType)]);
+  }
+
   return ts.factory.createNull();
 }
