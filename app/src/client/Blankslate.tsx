2cb483cc0a2656fd93c805fa895ef9854d346887
Tomas Vesely <wham@github.com>
2023-11-21 19:37:51 -0800

    bootstrap

--- app/src/client/Blankslate.tsx
+++ app/src/client/Blankslate.tsx
@@ -1,10 +1,13 @@
+import { Box } from "@primer/react";
 import { Blankslate as PrimerBlankslate } from "@primer/react/drafts";
 
 export function Blankslate() {
   return (
-    <PrimerBlankslate>
-      <PrimerBlankslate.Heading>Hello</PrimerBlankslate.Heading>
-      <PrimerBlankslate.PrimaryAction href="#">Create the first page</PrimerBlankslate.PrimaryAction>
-    </PrimerBlankslate>
+    <Box sx={{ flexGrow: 1 }}>
+      <PrimerBlankslate>
+        <PrimerBlankslate.Heading>Hello</PrimerBlankslate.Heading>
+        <PrimerBlankslate.PrimaryAction href="/bootstrap">Bootstrap</PrimerBlankslate.PrimaryAction>
+      </PrimerBlankslate>
+    </Box>
   );
 }
