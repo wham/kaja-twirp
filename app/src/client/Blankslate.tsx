0fe083ff3ed2411e0ae9e355157d83588b52f263
Tomas Vesely <wham@github.com>
2023-11-23 22:53:14 -0800

    working comms

--- app/src/client/Blankslate.tsx
+++ app/src/client/Blankslate.tsx
@@ -1,7 +1,22 @@
 import { Box } from "@primer/react";
 import { Blankslate as PrimerBlankslate } from "@primer/react/drafts";
+import { useEffect } from "react";
+import { FetchRPC } from "twirp-ts";
+import { ServerClientJSON } from "../shared/server.twirp";
 
 export function Blankslate() {
+  useEffect(() => {
+    const client = new ServerClientJSON(
+      FetchRPC({
+        baseUrl: "http://localhost:3000/api",
+      }),
+    );
+
+    client.Bootstrap({}).then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <PrimerBlankslate>
