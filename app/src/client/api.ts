ce1af10c60c119b59ab9e22e89fcaa2b39dc6e93
Tomas Vesely <wham@github.com>
2024-03-16 09:53:32 -0700

    Used protobuf-ts for api client

--- app/src/client/api.ts
+++ app/src/client/api.ts
@@ -1,12 +1,12 @@
-import { FetchRPC } from "twirp-ts";
-import { ApiClient, ApiClientJSON } from "../shared/api.twirp";
+import { TwirpFetchTransport } from "@protobuf-ts/twirp-transport";
+import { ApiClient } from "../shared/api.client";
 
 export function getApiClient(): ApiClient {
-  return new ApiClientJSON(
-    FetchRPC({
+  return new ApiClient(
+    new TwirpFetchTransport({
       baseUrl: getBaseUrl(),
-    })
-  )
+    }),
+  );
 }
 
 export function getBaseUrl(): string {
