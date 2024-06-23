e078e90beed54cea472c3729c13de972e5555594
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-06-23 09:29:41 -0700

    Build go after protoc

--- demo/internal/demo-app/enum.pb.go
+++ demo/internal/demo-app/enum.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.34.2
 // 	protoc        v5.27.1
-// source: lib/position.proto
+// source: lib/enum.proto
 
 package demo_app
 
@@ -50,11 +50,11 @@ func (x Position) String() string {
 }
 
 func (Position) Descriptor() protoreflect.EnumDescriptor {
-	return file_lib_position_proto_enumTypes[0].Descriptor()
+	return file_lib_enum_proto_enumTypes[0].Descriptor()
 }
 
 func (Position) Type() protoreflect.EnumType {
-	return &file_lib_position_proto_enumTypes[0]
+	return &file_lib_enum_proto_enumTypes[0]
 }
 
 func (x Position) Number() protoreflect.EnumNumber {
@@ -63,37 +63,37 @@ func (x Position) Number() protoreflect.EnumNumber {
 
 // Deprecated: Use Position.Descriptor instead.
 func (Position) EnumDescriptor() ([]byte, []int) {
-	return file_lib_position_proto_rawDescGZIP(), []int{0}
+	return file_lib_enum_proto_rawDescGZIP(), []int{0}
 }
 
-var File_lib_position_proto protoreflect.FileDescriptor
+var File_lib_enum_proto protoreflect.FileDescriptor
 
-var file_lib_position_proto_rawDesc = []byte{
-	0x0a, 0x12, 0x6c, 0x69, 0x62, 0x2f, 0x70, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x12, 0x03, 0x6c, 0x69, 0x62, 0x2a, 0x1f, 0x0a, 0x08, 0x50, 0x6f, 0x73,
-	0x69, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x07, 0x0a, 0x03, 0x54, 0x4f, 0x50, 0x10, 0x00, 0x12, 0x0a,
-	0x0a, 0x06, 0x42, 0x4f, 0x54, 0x54, 0x4f, 0x4d, 0x10, 0x01, 0x42, 0x13, 0x5a, 0x11, 0x69, 0x6e,
-	0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x64, 0x65, 0x6d, 0x6f, 0x2d, 0x61, 0x70, 0x70, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_lib_enum_proto_rawDesc = []byte{
+	0x0a, 0x0e, 0x6c, 0x69, 0x62, 0x2f, 0x65, 0x6e, 0x75, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x12, 0x03, 0x6c, 0x69, 0x62, 0x2a, 0x1f, 0x0a, 0x08, 0x50, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x6f,
+	0x6e, 0x12, 0x07, 0x0a, 0x03, 0x54, 0x4f, 0x50, 0x10, 0x00, 0x12, 0x0a, 0x0a, 0x06, 0x42, 0x4f,
+	0x54, 0x54, 0x4f, 0x4d, 0x10, 0x01, 0x42, 0x13, 0x5a, 0x11, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e,
+	0x61, 0x6c, 0x2f, 0x64, 0x65, 0x6d, 0x6f, 0x2d, 0x61, 0x70, 0x70, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
-	file_lib_position_proto_rawDescOnce sync.Once
-	file_lib_position_proto_rawDescData = file_lib_position_proto_rawDesc
+	file_lib_enum_proto_rawDescOnce sync.Once
+	file_lib_enum_proto_rawDescData = file_lib_enum_proto_rawDesc
 )
 
-func file_lib_position_proto_rawDescGZIP() []byte {
-	file_lib_position_proto_rawDescOnce.Do(func() {
-		file_lib_position_proto_rawDescData = protoimpl.X.CompressGZIP(file_lib_position_proto_rawDescData)
+func file_lib_enum_proto_rawDescGZIP() []byte {
+	file_lib_enum_proto_rawDescOnce.Do(func() {
+		file_lib_enum_proto_rawDescData = protoimpl.X.CompressGZIP(file_lib_enum_proto_rawDescData)
 	})
-	return file_lib_position_proto_rawDescData
+	return file_lib_enum_proto_rawDescData
 }
 
-var file_lib_position_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
-var file_lib_position_proto_goTypes = []any{
+var file_lib_enum_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_lib_enum_proto_goTypes = []any{
 	(Position)(0), // 0: lib.Position
 }
-var file_lib_position_proto_depIdxs = []int32{
+var file_lib_enum_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -101,27 +101,27 @@ var file_lib_position_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_lib_position_proto_init() }
-func file_lib_position_proto_init() {
-	if File_lib_position_proto != nil {
+func init() { file_lib_enum_proto_init() }
+func file_lib_enum_proto_init() {
+	if File_lib_enum_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_lib_position_proto_rawDesc,
+			RawDescriptor: file_lib_enum_proto_rawDesc,
 			NumEnums:      1,
 			NumMessages:   0,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_lib_position_proto_goTypes,
-		DependencyIndexes: file_lib_position_proto_depIdxs,
-		EnumInfos:         file_lib_position_proto_enumTypes,
+		GoTypes:           file_lib_enum_proto_goTypes,
+		DependencyIndexes: file_lib_enum_proto_depIdxs,
+		EnumInfos:         file_lib_enum_proto_enumTypes,
 	}.Build()
-	File_lib_position_proto = out.File
-	file_lib_position_proto_rawDesc = nil
-	file_lib_position_proto_goTypes = nil
-	file_lib_position_proto_depIdxs = nil
+	File_lib_enum_proto = out.File
+	file_lib_enum_proto_rawDesc = nil
+	file_lib_enum_proto_goTypes = nil
+	file_lib_enum_proto_depIdxs = nil
 }
