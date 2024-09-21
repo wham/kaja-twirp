01001cb8f5010246bb70034cc122bc45f7801d11
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-09-20 23:56:06 -0700

    added compiling view

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -2,41 +2,52 @@ import { Box, TreeView } from "@primer/react";
 import { Method, Project, Service, methodId } from "./project";
 
 interface SidebarProps {
-  project: Project;
-  onSelect: (service: Service, method: Method) => void;
+  project?: Project;
   currentMethod?: Method;
+  onSelect: (service: Service, method: Method) => void;
 }
 
-export function Sidebar({ project, onSelect, currentMethod }: SidebarProps) {
+export function Sidebar({ project, currentMethod, onSelect }: SidebarProps): JSX.Element {
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
       <Box sx={{ paddingX: 2, flexGrow: 1 }}>
         <nav aria-label="Services and methods">
           <TreeView aria-label="Services and methods">
-            {project.services.map((service, index) => {
-              return (
-                <TreeView.Item id={service.name} key={service.name} defaultExpanded={index === 0}>
-                  {service.name}
-                  <TreeView.SubTree>
-                    {service.methods.map((method) => {
-                      return (
-                        <TreeView.Item
-                          id={methodId(service, method)}
-                          key={methodId(service, method)}
-                          onSelect={() => onSelect(service, method)}
-                          current={currentMethod === method}
-                        >
-                          {method.name}
-                        </TreeView.Item>
-                      );
-                    })}
-                  </TreeView.SubTree>
-                </TreeView.Item>
-              );
-            })}
+            {project &&
+              project.services.map((service, index) => {
+                return (
+                  <TreeView.Item id={service.name} key={service.name} defaultExpanded={index === 0}>
+                    {service.name}
+                    <TreeView.SubTree>
+                      {service.methods.map((method) => {
+                        return (
+                          <TreeView.Item
+                            id={methodId(service, method)}
+                            key={methodId(service, method)}
+                            onSelect={() => onSelect(service, method)}
+                            current={currentMethod === method}
+                          >
+                            {method.name}
+                          </TreeView.Item>
+                        );
+                      })}
+                    </TreeView.SubTree>
+                  </TreeView.Item>
+                );
+              })}
+            {!project && LoadingTreeViewItem()}
           </TreeView>
         </nav>
       </Box>
     </Box>
+  );
+}
+
+function LoadingTreeViewItem(): JSX.Element {
+  return (
+    <TreeView.Item id="loading-tree-view-item" expanded={true}>
+      Loading...
+      <TreeView.SubTree state="loading" count={20} />
+    </TreeView.Item>
   );
 }
