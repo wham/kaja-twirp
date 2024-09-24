2d94da7a6fbf13584c8cfff46940f557186b4eab
Tomáš Veselý <wham@users.noreply.github.com>
2024-09-23 22:36:46 -0700

    Fixed console horizontal overflow (#53)
    Co-authored-by: Tomas Vesely <448809+wham@users.noreply.github.com>

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -9,36 +9,34 @@ interface SidebarProps {
 
 export function Sidebar({ project, currentMethod, onSelect }: SidebarProps) {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Box sx={{ paddingX: 2, flexGrow: 1 }}>
-        <nav aria-label="Services and methods">
-          <TreeView aria-label="Services and methods">
-            {project &&
-              project.services.map((service, index) => {
-                return (
-                  <TreeView.Item id={service.name} key={service.name} defaultExpanded={index === 0}>
-                    {service.name}
-                    <TreeView.SubTree>
-                      {service.methods.map((method) => {
-                        return (
-                          <TreeView.Item
-                            id={methodId(service, method)}
-                            key={methodId(service, method)}
-                            onSelect={() => onSelect(service, method)}
-                            current={currentMethod === method}
-                          >
-                            {method.name}
-                          </TreeView.Item>
-                        );
-                      })}
-                    </TreeView.SubTree>
-                  </TreeView.Item>
-                );
-              })}
-            {!project && LoadingTreeViewItem()}
-          </TreeView>
-        </nav>
-      </Box>
+    <Box sx={{ paddingX: 2 }}>
+      <nav aria-label="Services and methods">
+        <TreeView aria-label="Services and methods">
+          {project &&
+            project.services.map((service, index) => {
+              return (
+                <TreeView.Item id={service.name} key={service.name} defaultExpanded={index === 0}>
+                  {service.name}
+                  <TreeView.SubTree>
+                    {service.methods.map((method) => {
+                      return (
+                        <TreeView.Item
+                          id={methodId(service, method)}
+                          key={methodId(service, method)}
+                          onSelect={() => onSelect(service, method)}
+                          current={currentMethod === method}
+                        >
+                          {method.name}
+                        </TreeView.Item>
+                      );
+                    })}
+                  </TreeView.SubTree>
+                </TreeView.Item>
+              );
+            })}
+          {!project && LoadingTreeViewItem()}
+        </TreeView>
+      </nav>
     </Box>
   );
 }
