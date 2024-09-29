84abc618299b89ad27c1510a4a04d0d131731c11
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-09-29 09:20:22 -0700

    Added button

--- client/src/ControlBar.tsx
+++ client/src/ControlBar.tsx
@@ -1,5 +1,5 @@
-import { PlayIcon } from "@primer/octicons-react";
-import { Box, Button } from "@primer/react";
+import { PlayIcon, SyncIcon } from "@primer/octicons-react";
+import { Box, Button, IconButton } from "@primer/react";
 
 interface ControlBarProps {
   onRun: () => void;
@@ -7,10 +7,15 @@ interface ControlBarProps {
 
 export function ControlBar({ onRun }: ControlBarProps) {
   return (
-    <Box sx={{ padding: 1 }}>
-      <Button leadingVisual={PlayIcon} onClick={onRun} variant="primary" size="small">
-        Run
-      </Button>
+    <Box sx={{ padding: 1, display: "flex" }}>
+      <Box>
+        <Button leadingVisual={PlayIcon} onClick={onRun} variant="primary" size="small">
+          Run
+        </Button>
+      </Box>
+      <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "right" }}>
+        <IconButton icon={SyncIcon} aria-label="Recompile from .proto files" size="small" onClick={onRun} />
+      </Box>
     </Box>
   );
 }
