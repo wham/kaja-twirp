6b5eb38446a360fa5f39a643a84e1dedbc02c67a
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-02 22:03:43 -0700

    Basic

--- client/src/formatter/json.test.ts
+++ client/src/formatter/json.test.ts
@@ -1,6 +1,19 @@
 import { expect, test } from "vitest";
-import { formatAndColorizeJson } from "./json";
+import { stringifyAndColorize } from "./json";
 
-test("formatAndColorizeJson", () => {
-  expect(formatAndColorizeJson("{ a: 1 }")).toBe("");
+test("formatAndColorize", () => {
+  expect(stringifyAndColorize("hi>")).toBe(`<span style="color: #ce9178;">"hi&gt;"</span>`);
+  expect(stringifyAndColorize(10)).toBe(`<span style="color: #b5cea8;">10</span>`);
+  expect(stringifyAndColorize(10n)).toBe(`<span style="color: #b5cea8;">10</span>`);
+  expect(stringifyAndColorize(true)).toBe(`<span style="color: #569cd6;">true</span>`);
+  expect(stringifyAndColorize(null)).toBe(`<span style="color: #b5cea8;">null</span>`);
+  expect(stringifyAndColorize(undefined)).toBe(`<span style="color: #b5cea8;">undefined</span>`);
+  expect(stringifyAndColorize(["hi>", 10])).toBe(
+    `<span style="color: #b5cea8;">[</span>, <span style="color: #ce9178;">"hi&gt;"</span>, <span style="color: #b5cea8;">10</span><span style="color: #b5cea8;">]</span>`,
+  );
+  expect(
+    stringifyAndColorize({ string: "hi>", number: 10, boolean: true, null: null, undefined: undefined, array: [["hi>", 10]], object: { next: "level" } }),
+  ).toBe(
+    `<span style="color: #b5cea8;">[</span>, <span style="color: #ce9178;">"hi&gt;"</span>, <span style="color: #b5cea8;">10</span><span style="color: #b5cea8;">]</span>`,
+  );
 });
--- client/src/formatter/json.ts
+++ client/src/formatter/json.ts
@@ -38,3 +38,54 @@ export function formatAndColorizeJson(json: string): string {
     })
     .join("\n");
 }
+
+export function stringifyAndColorize(value: any): string {
+  if (value === null) {
+    return `<span style="color: #b5cea8;">null</span>`;
+  }
+
+  if (value === undefined) {
+    return `<span style="color: #b5cea8;">undefined</span>`;
+  }
+
+  if (Array.isArray(value)) {
+    let output = `<span style="color: #b5cea8;">[</span>`;
+    for (let i = 0; i < value.length; i++) {
+      output += ", " + stringifyAndColorize(value[i]);
+    }
+    output += `<span style="color: #b5cea8;">]</span>`;
+
+    return output;
+  }
+
+  if (Object.prototype.toString.call(value) === "[object Object]") {
+    let output = `<span style="color: #b5cea8;">{</span>`;
+    for (const key in value) {
+      output += `<span style="color: #d4d4d4;">"${key}"</span>: ` + stringifyAndColorize(value[key]);
+    }
+    output += `<span style="color: #b5cea8;">}</span>`;
+
+    return output;
+  }
+
+  const type = typeof value;
+
+  switch (type) {
+    case "string":
+      return `<span style="color: #ce9178;">"${escapeHtml(value)}"</span>`;
+    case "number":
+    case "bigint":
+      return `<span style="color: #b5cea8;">${value}</span>`;
+    case "boolean":
+      return `<span style="color: #569cd6;">${value}</span>`;
+    case "object":
+    case "undefined":
+      return `<span style="color: magenta;">${value}</span>`;
+    default:
+      return value + "?(" + type + ")";
+  }
+}
+
+function escapeHtml(string: string) {
+  return string.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
