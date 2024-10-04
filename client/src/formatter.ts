1c63cab582576edc8a0b41dae8c4d475ea788b1c
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-10-03 23:10:19 -0700

    dry

--- client/src/formatter.ts
+++ client/src/formatter.ts
@@ -4,25 +4,21 @@ import prettierPluginEsTree from "prettier/plugins/estree";
 import prettierPluginTypescript from "prettier/plugins/typescript";
 
 export async function formatJson(code: string): Promise<string> {
-  return prettier
-    .format(code, { parser: "json", plugins: [prettierPluginBabel, prettierPluginEsTree] })
-    .then((formattedCode) => {
-      return formattedCode;
-    })
-    .catch((error) => {
-      console.warn("Failed to format JSON", error);
-      return code;
-    });
+  return format(code, "json", [prettierPluginBabel, prettierPluginEsTree]);
 }
 
 export async function formatTypeScript(code: string): Promise<string> {
+  return format(code, "typescript", [prettierPluginTypescript, prettierPluginEsTree]);
+}
+
+function format(code: string, parser: string, plugins: prettier.Plugin[]): Promise<string> {
   return prettier
-    .format(code, { parser: "typescript", plugins: [prettierPluginTypescript, prettierPluginEsTree] })
+    .format(code, { parser, plugins })
     .then((formattedCode) => {
       return formattedCode;
     })
     .catch((error) => {
-      console.warn("Failed to format TypeScript", error);
+      console.warn("Failed to format " + parser, error);
       return code;
     });
 }
