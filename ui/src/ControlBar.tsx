8e7ed5da4b7c1bef01018af4b5439d36b4a945ab
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-11-27 22:00:48 -0800

    Done

--- ui/src/ControlBar.tsx
+++ ui/src/ControlBar.tsx
@@ -1,5 +1,5 @@
 import { PlayIcon } from "@primer/octicons-react";
-import { Box, Button } from "@primer/react";
+import { Box, Button, Tooltip } from "@primer/react";
 
 interface ControlBarProps {
   onRun: () => void;
@@ -7,10 +7,25 @@ interface ControlBarProps {
 
 export function ControlBar({ onRun }: ControlBarProps) {
   return (
-    <Box sx={{ padding: 1 }}>
-      <Button leadingVisual={PlayIcon} onClick={onRun} variant="primary" size="small">
-        Run
-      </Button>
+    <Box sx={{ position: "absolute", top: "20px", right: "40px", zIndex: 1 }}>
+      <Tooltip aria-label="Run" direction="s">
+        <Button
+          leadingVisual={() => <PlayIcon size={100} fill="#1a7f37" />}
+          onClick={onRun}
+          variant="invisible"
+          size="large"
+          sx={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "50%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 0,
+            backgroundColor: "transparent",
+          }}
+        />
+      </Tooltip>
     </Box>
   );
 }
